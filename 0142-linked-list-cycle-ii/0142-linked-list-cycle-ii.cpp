19ff6ff57c0b7c4faa61aa004788c6c8d27797c8
Meenakshisingh2256 <meenakshichauhan2505@gmail.com>
2026-01-31 13:01:06 +0530

    Time: 5 ms (83.37%), Space: 11.5 MB (23.89%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,17 +6,36 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         unordered_set<ListNode *> visit;
+//         ListNode *temp=head;
+//         while(temp!=NULL){
+//             if(visit.count(temp)==1){
+//                 return temp;
+//             } visit.insert(temp);
+//             temp=temp->next;
+             
+//         } return nullptr;
+//     }
+// }; BRUTE FORCE METHOD
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode *> visit;
-        ListNode *temp=head;
-        while(temp!=NULL){
-            if(visit.count(temp)==1){
-                return temp;
-            } visit.insert(temp);
-            temp=temp->next;
-             
-        } return nullptr;
+        ListNode *slow=head;
+        ListNode * fast=head; 
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(fast==slow){ 
+                break;
+            }
+        } if(fast==nullptr ||fast->next==nullptr )return nullptr;
+        slow=head;
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        } return fast;
     }
 };
